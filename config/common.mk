0e10376c77923e88e4bbbb1f99f23b9a88bb7e01
ElDainosor <eldainosor@gmail.com>
2018-11-21 14:06:10 +0000

    derp: Add weather code to use the new WeatherClient

--- config/common.mk
+++ config/common.mk
@@ -135,6 +135,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	vendor/derp/prebuilt/common/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
 
+# Weather
+PRODUCT_COPY_FILES += \
+    vendor/derp/prebuilt/common/etc/permissions/com.android.providers.weather.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.android.providers.weather.xml \
+    vendor/derp/prebuilt/common/etc/default-permissions/com.android.providers.weather.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/default-permissions/com.android.providers.weather.xml
+
 # Fonts to copy
 PRODUCT_COPY_FILES += \
     vendor/derp/prebuilt/fonts/fonts_shishufied.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
