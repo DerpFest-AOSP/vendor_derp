89fdf24039ff60e4deb74af087fec28574625515
LuK1337 <priv.luk@gmail.com>
2019-10-24 02:33:34 +0200

    derp: Bring back Android Beam
    * While this feature is deprecated all code
      for it is still alive ^.^
    
    Change-Id: I36fac374e43ceddab1438255654bb541937041ff

--- config/common.mk
+++ config/common.mk
@@ -219,6 +219,10 @@ PRODUCT_COPY_FILES += \
     vendor/derp/prebuilt/fonts/fontagev3/Volte-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Volte-Bold.ttf \
     vendor/derp/prebuilt/fonts/fontagev3/Volte-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Volte-Medium.ttf
 
+# Enable Android Beam on all targets
+PRODUCT_COPY_FILES += \
+    vendor/derp/prebuilt/common/etc/permissions/android.software.nfc.beam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.nfc.beam.xml
+
 # Strip the local variable table and the local variable type table to reduce
 # the size of the system image. This has no bearing on stack traces, but will
 # leave less information available via JDWP.
