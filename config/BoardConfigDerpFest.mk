6b6ab4b52161d690a9063f301746466d031752cd
Arseniy Graur <argraur@aosip.dev>
2020-02-15 21:15:00 +0000

    [1/2] derp: Add a guard for all kernel-related tasks to check for prebuilt kernel
    * Do not use generated_kernel_headers if prebuilt kernel
    * Do not post tasks in kernel.mk if prebuilt kernel
    
    Signed-off-by: Arseniy Graur <argraur@aosip.dev>

--- config/BoardConfigDerpFest.mk
+++ config/BoardConfigDerpFest.mk
@@ -1,5 +1,9 @@
 # Kernel
+ifeq ($(LOCAL_KERNEL),)
 include vendor/derp/config/BoardConfigKernel.mk
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/derp/build/soong/generator
+endif
 
 # Qcom-specific bits
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
