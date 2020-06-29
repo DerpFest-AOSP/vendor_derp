2e5ae4a0e6ba33f82e3cde55502b1cfa7b47ae78
Alexander Koskovich <zvnexus@outlook.com>
2020-06-28 22:24:53 -0400

    soong: Add TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS.
    Change-Id: Ibb4202eae237cc9f167707c45dd86a313d95d7be

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -39,6 +39,7 @@ SOONG_CONFIG_derpGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_fod_lib \
+    target_uses_prebuilt_dynamic_partitions \
     uses_camera_parameter_lib
 
 ifneq ($(TARGET_FORCE_BUILD_FINGERPRINT),)
@@ -72,6 +73,7 @@ SOONG_CONFIG_derpGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA
 SOONG_CONFIG_derpGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_derpGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_derpGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
+SOONG_CONFIG_derpGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 SOONG_CONFIG_derpNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_derpQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
 SOONG_CONFIG_derpQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
