d73dde8a2546977cb0717a42b73092312224ff26
Dyneteve <dyneteve@hentaios.com>
2020-09-09 11:11:21 +0200

    derp: build: tools: Don't check elf files

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -403,6 +403,7 @@ function write_blueprint_packages() {
                 printf '\t\t},\n'
             fi
             printf '\t},\n'
+            printf '\tcheck_elf_files: false,\n' "$EXTRA"
             if [ "$EXTRA" != "none" ]; then
                 printf '\tcompile_multilib: "%s",\n' "$EXTRA"
             fi
