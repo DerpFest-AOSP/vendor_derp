6f820d1b501377a725731666e59339b478c55275
NurKeinNeid <mralexman3000@gmail.com>
2020-09-15 14:25:24 +0200

    derp: Import build additions from DerpLab
    * HEAD at DerpLab/platform_vendor_aosip@cc68d1bc0bb6ba36b026998d883f8c17ea68c0ee
    * all credits to LOS/AOSiP
    
    Change-Id: I83ceb30c3d03212876a328c57bcf038e998fa007
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

---
+++ build/core/config.mk
@@ -0,0 +1,24 @@
+
+# Copyright (C) 2018 DerpFest
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
+SELINUX_IGNORE_NEVERALLOWS := true
+endif
+
+# Rules for QCOM targets
+include $(TOPDIR)vendor/derp/build/core/qcom_target.mk
+
+BUILD_RRO_SYSTEM_PACKAGE := $(TOP)/vendor/derp/build/core/system_rro.mk
---
+++ build/core/definitions.mk
@@ -0,0 +1,12 @@
+#
+# DerpFest-specific macros
+#
+define uniq
+$(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
+endef
+ 
+# Include board/platform macros
+include vendor/derp/build/core/utils.mk
+
+# Include vendor platform definitions
+include vendor/derp/build/core/vendor/*.mk
