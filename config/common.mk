LineageOS/android_vendor_lineage

Use component-override to configure GMS/Vending
Change-Id: I0a2528fccb54befaed8f598d79cf1b195cb34838

config/common.mk
@@ -46,6 +46,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/lineage-sysconfig.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/lineage-sysconfig.xml
 
+# Lineage-specific component overrides
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/component-overrides.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
+
 # Copy all Lineage-specific init rc files
 $(foreach f,$(wildcard vendor/lineage/prebuilt/common/etc/init/*.rc),\
 	$(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_SYSTEM)/etc/init/$(notdir $f)))

DerpFest-AOSP/vendor_derp

config/common.mk
@@ -61,6 +61,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/derp/config/backup.xml:system/etc/sysconfig/backup.xml
 
+# DerpFest-specific component overrides
+PRODUCT_COPY_FILES += \
+    vendor/derp/config/component-overrides.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
+
 # Configs
 PRODUCT_COPY_FILES += \
     vendor/derp/prebuilt/common/etc/sysconfig/dialer_experience.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/dialer_experience.xml \