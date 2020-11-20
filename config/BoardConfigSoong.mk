d4e40ac25ebcd3f2d7f894aa00222487fffef2e3
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 12:50:43 -0300

    derp: Allow forcing custom build fingerprint [1/2]
    Change-Id: I783a19829cb9d655f8abaeafa14f1f3d6a12ed20

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -40,6 +40,10 @@ SOONG_CONFIG_derpGlobalVars += \
     target_surfaceflinger_fod_lib \
     uses_camera_parameter_lib
 
+ifneq ($(TARGET_FORCE_BUILD_FINGERPRINT),)
+SOONG_CONFIG_derpGlobalVars += force_build_fingerprint
+endif
+
 SOONG_CONFIG_NAMESPACES += derpNvidiaVars
 SOONG_CONFIG_derpNvidiaVars += \
     uses_nv_enhancements
@@ -91,6 +95,11 @@ SOONG_CONFIG_derpGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_L
 SOONG_CONFIG_derpGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_derpGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
 SOONG_CONFIG_derpGlobalVars_uses_camera_parameter_lib := $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)
+
+ifneq ($(TARGET_FORCE_BUILD_FINGERPRINT),)
+SOONG_CONFIG_derpGlobalVars_force_build_fingerprint := $(TARGET_FORCE_BUILD_FINGERPRINT)
+endif
+
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_derpQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
 endif
