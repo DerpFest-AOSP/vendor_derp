e818244f1e5757de721960c277f1fdc941fbeee9
NurKeinNeid <mralexman3000@gmail.com>
2020-11-26 03:21:45 +0100

    derp: prebuilts: Remove old apns config
    Change-Id: I8283699470a7bcb31c771c9aa770a825b4e82523
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,22 +1,2 @@
 # Inherit common stuff
 $(call inherit-product, vendor/derp/config/common.mk)
-
-# World APN list
-PRODUCT_PACKAGES += \
-    apns-conf.xml
-
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/derp/prebuilt/common/etc/spn-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/spn-conf.xml
-
-# Selective SPN list for operator number who has the problem.
-PRODUCT_COPY_FILES += \
-    vendor/derp/prebuilt/common/etc/selective-spn-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/selective-spn-conf.xml
-
-# SIM Toolkit
-PRODUCT_PACKAGES += \
-    Stk
-
-# Sensitive Phone Numbers list
-PRODUCT_COPY_FILES += \
-    vendor/derp/prebuilt/common/etc/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
