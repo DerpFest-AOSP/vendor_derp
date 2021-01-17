c47e9b2c4d05e343e56c25425b676d8139611448
jhenrique09 <jhenrique09.mcz@hotmail.com>
2021-01-17 07:30:08 -0300

    derp: Remove force build fingerprint
    Change-Id: I2c5f32a9e0004e4138ae003f81f2a3b950d139ff
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/BoardConfigDerpFest.mk
+++ config/BoardConfigDerpFest.mk
@@ -1,6 +1,3 @@
-# Safetynet
-TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
-
 include vendor/derp/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
