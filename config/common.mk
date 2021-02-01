743dc77816fc2916f9c388075accc297f1a3a502
Alexandru Scurtu <sasha.scurtu@gmail.com>
2021-02-01 19:40:58 +0200

    derp: config: decrease gboard ime height
    after adding gesture length customization, padding between keyboard and navbar became excessive.
    set this prop to avoid this issue
    todo: find a proper solution
    
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- config/common.mk
+++ config/common.mk
@@ -15,6 +15,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Gboard configuration
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.kb_pad_port_b=1 \
     ro.com.google.ime.theme_id=5 \
     ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
 
