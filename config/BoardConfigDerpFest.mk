acb7f0f71b8e76c7e69a0727ac068aa5f7dffdfc
NurKeinNeid <mralexman3000@gmail.com>
2021-02-02 23:31:04 +0100

    derp: Update redfin fingerprint to Feb
    Change-Id: I4274026943ce64a51a30c3fbf6cc85eec853ad5g
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/BoardConfigDerpFest.mk
+++ config/BoardConfigDerpFest.mk
@@ -1,5 +1,5 @@
 # Safetynet
-TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys
 
 include vendor/derp/config/BoardConfigKernel.mk
 
