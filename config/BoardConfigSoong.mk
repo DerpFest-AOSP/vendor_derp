7b7fbaaec63ea479af3f87f68eeb6ddfe70ad44c
Nolen Johnson <johnsonnolen@gmail.com>
2021-02-22 12:36:48 -0500

    soong: Add TARGET_LEGACY_HW_DISK_ENCRYPTION conditional
    Change-Id: I2c67fe632d415353273839493a86cdd75bd93b23

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -54,6 +54,7 @@ SOONG_CONFIG_derpNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += derpQcomVars
 SOONG_CONFIG_derpQcomVars += \
+    legacy_hw_disk_encryption \
     should_wait_for_qsee \
     supports_extended_compress_format \
     supports_hw_fde \
@@ -78,6 +79,7 @@ SOONG_CONFIG_derpGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNOR
 SOONG_CONFIG_derpGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_derpGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 SOONG_CONFIG_derpNvidiaVars_uses_nv_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
+SOONG_CONFIG_derpQcomVars_legacy_hw_disk_encryption := $(TARGET_LEGACY_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_derpQcomVars_should_wait_for_qsee := $(TARGET_KEYMASTER_WAIT_FOR_QSEE)
 SOONG_CONFIG_derpQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_derpQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
