f5fd786a72de878e8f57bb39e6caf5c0493d71f7
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-25 16:02:22 +0530

    overlay/props: Properly enable iorap tracing and readahead
     * iorapd reads these configs through device_config so we must
       enable it the same way

--- config/common.mk
+++ config/common.mk
@@ -266,6 +266,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     iorapd.perfetto.enable=true \
     iorapd.readahead.enable=true \
+    persist.device_config.runtime_native_boot.iorap_perfetto_enable=true \
+    persist.device_config.runtime_native_boot.iorap_readahead_enable=true \
     ro.iorapd.enable=true
 
 # Strip the local variable table and the local variable type table to reduce
