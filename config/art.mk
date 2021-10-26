9de7b076a6cc524d35c0b5a997c56ec94d2ae03f
Jason Edson <jaysonedson@gmail.com>
2021-10-26 04:45:24 -0700

    art: Use dexopt profiles if the repo exists
    Change-Id: Ia62d787037e4a01a5a08b5407d4109b86678b6f0
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- config/art.mk
+++ config/art.mk
@@ -5,3 +5,10 @@ ART_BUILD_TARGET_DEBUG := false
 ART_BUILD_HOST_NDEBUG := true
 ART_BUILD_HOST_DEBUG := false
 USE_DEX2OAT_DEBUG := false
+
+# Dexopt profiles
+profile_path := vendor/dexopt_profiles
+ifneq ($(wildcard $(profile_path)),)
+PRODUCT_DEX_PREOPT_PROFILE_DIR := $(profile_path)
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
+endif
