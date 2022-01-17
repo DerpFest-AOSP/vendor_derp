534214470e5493565de1bc3e52df68badf15489a
NurKeinNeid <mralexman3000@gmail.com>
2022-01-18 00:25:41 +0100

    Revert "prebuilt: Add fonts customization from Pixel"
    This reverts commit 919f083d90ee2e05d5656487b4d544932266ee0f.

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,10 +3,6 @@ $(call inherit-product, vendor/derp/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml
-
 # RRO Overlays
 PRODUCT_PACKAGES += \
     NavigationBarModeGesturalOverlayFS
