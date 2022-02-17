1fad4555db93874f198671ba4fd3593d6706ce74
Daniel Micay <danielmicay@gmail.com>
2022-02-17 21:28:53 +0100

    [temporary] config: disable broken AvoidAppsInCutoutOverlay

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -28,7 +28,6 @@ PRODUCT_PACKAGES += \
     DisplayCutoutEmulationNarrowOverlay \
     DisplayCutoutEmulationWideOverlay \
     NoCutoutOverlay \
-    AvoidAppsInCutoutOverlay \
     NavigationBarMode2ButtonOverlay
 
 # Lawnicons
