a029b29742f05f4f4157abf3be25a6bc921ad6c0
Bruno Martins <bgcngm@gmail.com>
2022-02-19 19:01:10 +0100

    Rename init.derp-system.rc and move to /system_ext
    Change-Id: Ife4f9467b6aa2d656a2df34cc9df5dafef304348

--- config/common.mk
+++ config/common.mk
@@ -52,7 +52,7 @@ endif
 
 # DerpFest-specific init rc file
 PRODUCT_COPY_FILES += \
-    vendor/derp/prebuilt/common/etc/init/init.derp-system.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.derp-system.rc
+    vendor/derp/prebuilt/common/etc/init/init.derp-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.derp-system_ext.rc
 
 # Enable Android Beam on all targets
 PRODUCT_COPY_FILES += \
