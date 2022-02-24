2e5f908645649eae29c8361c73feb06956af9181
Albert I <kras@raphielgang.org>
2022-02-24 21:18:49 +0800

    derp: Fix inclusion of `ro.ota.allow_downgrade` property
    This shouldn't have been guarded by A/B flag.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- config/common.mk
+++ config/common.mk
@@ -43,11 +43,11 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/bin/backuptool_ab.sh \
     system/bin/backuptool_ab.functions \
     system/bin/backuptool_postinstall.sh
+endif
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
-endif
 endif
 
 # DerpFest-specific init rc file
