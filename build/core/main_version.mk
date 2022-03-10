797bafa2a7f2f62420b39e060c2046cb43bf3d50
jhenrique09 <jhsv09@gmail.com>
2022-03-10 23:08:59 +0530

    derp: Add CUSTOM_SECURITY_PATCH prop
    Change-Id: I0e690874ee9d3a783719494ea0e5bc3ece6cbf13

---
+++ build/core/main_version.mk
@@ -0,0 +1,6 @@
+# Custom security patch
+CUSTOM_SECURITY_PATCH := 2024-10-05
+
+# DerpFest specific props
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    org.derpfest.build_security_patch=$(CUSTOM_SECURITY_PATCH)
