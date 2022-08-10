109b15616fcf17410b71dc8950b67a082dd27edd
jhenrique09 <jhsv09@gmail.com>
2022-08-10 15:54:35 +0000

    derp: Add CUSTOM_SECURITY_PATCH prop
    Change-Id: I0e690874ee9d3a783719494ea0e5bc3ece6cbf13

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,3 +1,7 @@
+# Custom security patch
+CUSTOM_SECURITY_PATCH := 2022-08-05
+
 # DerpFest Android 12L Specific Props
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.derp.settings.android_version=12L
+    ro.derp.settings.android_version=12L \
+    org.derpfest.build_security_patch=$(CUSTOM_SECURITY_PATCH)
