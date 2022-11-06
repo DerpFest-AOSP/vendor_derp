7b4a67048be5ded6743786b4ef92df1216e7af99
David Sehr <sehr@google.com>
2022-11-06 15:05:50 +0100

    config: enable dex2oat64 on 64-bit targets
    Bug: 153380900
    Test: boot and install application.
    Change-Id: Iac14c028c1c5c7aa4a9173d51d63ef5834ba690a

--- config/art.mk
+++ config/art.mk
@@ -6,6 +6,12 @@ ART_BUILD_HOST_NDEBUG := true
 ART_BUILD_HOST_DEBUG := false
 USE_DEX2OAT_DEBUG := false
 
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS), true)
+# Use 64-bit dex2oat for better dexopt time.
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat64.enabled=true
+endif
+
 # Dexopt profiles
 profile_path := vendor/dexopt_profiles
 ifneq ($(wildcard $(profile_path)),)
