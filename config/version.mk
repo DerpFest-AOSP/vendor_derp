048964933fcfacd97dc802db8b7be28acad74ba8
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-02 21:57:53 +0200

    config: Conditionally enable dev keys signing

--- config/version.mk
+++ config/version.mk
@@ -44,3 +44,17 @@ ifneq ($(OVERRIDE_OTA_CHANNEL),)
 PRODUCT_SYSTEM_PROPERTIES += \
     derp.updater.uri=$(OVERRIDE_OTA_CHANNEL)
 endif
+
+# Signing
+ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard .android-certs/releasekey.pk8))
+PRODUCT_DEFAULT_DEV_CERTIFICATE := .android-certs/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard .android-certs/verity.pk8))
+PRODUCT_VERITY_SIGNING_KEY := .android-certs/verity
+endif
+ifneq (,$(wildcard .android-certs/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := .android-certs/otakey.x509.pem
+endif
+endif
