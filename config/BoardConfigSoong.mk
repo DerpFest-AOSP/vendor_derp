5f337fb9537c7c86ae6463175ddffe5331b635af
ShevT <evgeny.shishkov@gmail.com>
2023-05-23 19:20:31 +0300

    vendor: soong: Add camera_needs_client_info_lib_oplus
    Change-Id: I075cdcae3b74104ba6923c151b1e92e4513e8996

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -32,6 +32,7 @@ SOONG_CONFIG_derpGlobalVars += \
     additional_gralloc_10_usage_bits \
     camera_override_format_from_reserved \
     gralloc_handle_has_custom_content_md_reserved_size \
+    camera_needs_client_info_lib_oplus \
     gralloc_handle_has_reserved_size \
     target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
@@ -73,6 +74,7 @@ SOONG_CONFIG_derpNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_E
 SOONG_CONFIG_derpQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_derpQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_derpQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
+SOONG_CONFIG_customGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
