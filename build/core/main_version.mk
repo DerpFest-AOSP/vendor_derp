c8edf014744ba47da714c3eef32e0fbc2c4f96e8
NurKeinNeid <mralexman3000@gmail.com>
2023-10-05 12:30:43 +0200

    Revert "derp: Add CUSTOM_SECURITY_PATCH prop"
    This reverts commit 8792e9a0600e84c074fdf2d824c5dca132528c31.

--- build/core/main_version.mk
+++
@@ -1,6 +0,0 @@
-# Custom security patch
-CUSTOM_SECURITY_PATCH := 2023-10-05
-
-# DerpFest specific props
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    org.derpfest.build_security_patch=$(CUSTOM_SECURITY_PATCH)
