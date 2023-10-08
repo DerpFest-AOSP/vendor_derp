84b1a397d7f69824eadfc3fa0303c77c13d85821
NurKeinNeid <mralexman3000@gmail.com>
2023-10-08 11:27:00 +0200

    derp: Drop bootanimations
    Change-Id: Ic997738f59b8959852856417a87634638bb3f063
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -106,15 +106,6 @@ PRODUCT_SYSTEM_PROPERTIES += \
 TARGET_SCREEN_WIDTH ?= 1080
 TARGET_SCREEN_HEIGHT ?= 1920
 
-# Boot Animation
-ifeq ($(USE_LEGACY_BOOTANIMATION), true)
-PRODUCT_COPY_FILES += \
-    vendor/derp/bootanimation/bootanimation_legacy.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-else
-PRODUCT_COPY_FILES += \
-    vendor/derp/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-endif
-
 PRODUCT_COPY_FILES += \
     vendor/derp/prebuilt/common/etc/init/init.derp-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.derp-updater.rc
 
