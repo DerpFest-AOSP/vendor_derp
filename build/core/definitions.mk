f7a9e9e083f51228de9925af9d040ce0317fc899
Bruno Martins <bgcngm@gmail.com>
2023-10-25 00:53:43 +0100

    derp: Move all QCOM board configs to hardware/qcom-caf/common
    Change-Id: I6ead35cf884255a8897a3e3e27fc7936d11be616

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -7,6 +7,3 @@ endef
 
 # Include board/platform macros
 include vendor/derp/build/core/utils.mk
-
-# Include vendor platform definitions
-include vendor/derp/build/core/vendor/*.mk
--- config/BoardConfigDerpFest.mk
+++ config/BoardConfigDerpFest.mk
@@ -1,7 +1,7 @@
 include vendor/derp/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/derp/config/BoardConfigQcom.mk
+include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 include vendor/derp/config/BoardConfigSoong.mk
