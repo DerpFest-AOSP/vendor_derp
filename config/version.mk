495a1967a6c9bf0baf8a6bb7a6e001d876804949
Ido Ben-Hur <idoybh2@gmail.com>
2024-03-13 13:35:09 +0200

    version: Sign every build variant but eng

--- config/version.mk
+++ config/version.mk
@@ -46,7 +46,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
 endif
 
 # Signing
-ifeq (user,$(TARGET_BUILD_VARIANT))
+ifneq (eng,$(TARGET_BUILD_VARIANT))
 ifneq (,$(wildcard vendor/derp/signing/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/derp/signing/keys/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
