7e9328739751932cbf7a08b3399656e66566b85c
NurKeinNeid <mralexman3000@gmail.com>
2024-04-14 19:02:06 +0200

    tasks: derp: Don't touch aosp otapackage target
    Change-Id: Ie2147893a1551b736880913334a3c134a17ca80d
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- build/tasks/derp.mk
+++ build/tasks/derp.mk
@@ -2,8 +2,8 @@ DERP_TARGET_PACKAGE := $(PRODUCT_OUT)/DerpFest-$(DERP_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
-.PHONY: derp bacon
-derp: otapackage
+.PHONY: derp
+derp: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(DERP_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(DERP_TARGET_PACKAGE) | cut -d ' ' -f1 > $(DERP_TARGET_PACKAGE).sha256sum
 	$(hide) ./vendor/derp/tools/generate_json_build_info.sh $(DERP_TARGET_PACKAGE)
@@ -30,5 +30,3 @@ derp: otapackage
 	@echo -e "sha256: `cat $(DERP_TARGET_PACKAGE).sha256sum | cut -d ' ' -f 1`"
 	@echo -e "size:`ls -lah $(DERP_TARGET_PACKAGE) | cut -d ' ' -f 5`"
 	@echo -e ""
-
-bacon: derp
