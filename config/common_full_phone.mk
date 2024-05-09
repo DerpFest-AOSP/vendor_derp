0dcd61c79d7d5a8d0b7064ebc908e28660e0cf81
NurKeinNeid <mralexman3000@gmail.com>
2024-05-09 20:29:29 +0200

    config: Update gms flags and inheritance
    Change-Id: I8c16eae8eed75f4fea50c8e213eb938fa7e76726
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -16,16 +16,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/derp/overlay/dictionaries
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode?=true
 
-# GApps
+# GMS
 WITH_GMS := true
-# Inherit from GMS product config
-ifeq ($(TARGET_USES_MINI_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_mini.mk)
-else ifeq ($(TARGET_USES_PICO_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_pico.mk)
-else
-$(call inherit-product, vendor/gms/gms_full.mk)
-endif
+$(call inherit-product, vendor/gms/products/gms.mk)
 
 # Pixel Framework
 $(call inherit-product, vendor/pixel-framework/config.mk)
