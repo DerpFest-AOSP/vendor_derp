LineageOS/android_vendor_lineage

config: tablet: Deduplicate full tablet configuration
Change-Id: I12cc09d4fca28d063cea76301de287526bfec570

config/common_full_tablet.mk
@@ -1,10 +1,7 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
-
 # Inherit mobile full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
-# Settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.settings.large_screen_opt.enabled=true
+# Inherit full tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/full_tablet.mk)
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)

config/common_full_tablet_wifionly.mk
@@ -1,10 +1,7 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
-
 # Inherit mobile full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
-# Settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.settings.large_screen_opt.enabled=true
+# Inherit full tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/full_tablet.mk)
 
 $(call inherit-product, vendor/lineage/config/wifionly.mk)

DerpFest-AOSP/vendor_derp

config/common_full_tablet.mk
@@ -1,5 +1,3 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
-
 # Inherit mobile full common DerpFest stuff
 $(call inherit-product, vendor/derp/config/common_mobile_full.mk)
 
@@ -17,9 +15,8 @@ endif
 # Pixel Framework
 $(call inherit-product, vendor/pixel-framework/config.mk)
 
-# Settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.settings.large_screen_opt.enabled=true
+# Inherit full tablet common DerpFest stuff
+$(call inherit-product, vendor/derp/config/full_tablet.mk)
 
 # Inherit from telephony config
 $(call inherit-product, vendor/derp/config/telephony.mk)

config/common_full_tablet_wifionly.mk
@@ -11,8 +11,7 @@ $(call inherit-product, vendor/gms/gms_full_tablet_wifionly.mk)
 # Pixel Framework
 $(call inherit-product, vendor/pixel-framework/config.mk)
 
-# Settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.settings.large_screen_opt.enabled=true
+# Inherit full tablet common DerpFest stuff
+$(call inherit-product, vendor/derp/config/full_tablet.mk)
 
 $(call inherit-product, vendor/derp/config/wifionly.mk)