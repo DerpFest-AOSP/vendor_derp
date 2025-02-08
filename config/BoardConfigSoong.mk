5e24271091d823bb8a6163634d7adf150ee3b077
borbelyvince <borbely.vince.11@gmail.com>
2025-02-08 19:16:40 +0100

    Revert "config: Changes for libfmjni bp conversion"
    This reverts commit 2c2c265074040f58127a94aff8805529dbaed1b6.
    
    Reason for revert: The soong namespaces can be removed after reworking
    libfmjni to use select() with filegroups and header libs for qcom/slsi.
    Broadcom is not used in any recent shipping device and mtk can continue
    using prebuilt for now.
    
    Change-Id: I078b321c7c55c3d7e5cd97e2664208c3d44f513a

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -64,7 +64,6 @@ SOONG_CONFIG_derpNvidiaVars += \
 
 SOONG_CONFIG_NAMESPACES += derpQcomVars
 SOONG_CONFIG_derpQcomVars += \
-    no_fm_firmware \
     qti_vibrator_effect_lib \
     qti_vibrator_use_effect_stream \
     supports_extended_compress_format \
@@ -85,7 +84,6 @@ SOONG_CONFIG_derpGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_derpGlobalVars_uses_oplus_touch := $(TARGET_USES_OPLUS_TOUCH)
 SOONG_CONFIG_derpGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_derpNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
-SOONG_CONFIG_derpQcomVars_no_fm_firmware := $(TARGET_QCOM_NO_FM_FIRMWARE)
 SOONG_CONFIG_derpQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
 SOONG_CONFIG_derpQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_derpQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
@@ -139,18 +137,3 @@ else
 SOONG_CONFIG_derpQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
 endif
 SOONG_CONFIG_derpQcomVars_qti_vibrator_effect_lib := $(TARGET_QTI_VIBRATOR_EFFECT_LIB)
-
-# libfmjni
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
-    PRODUCT_SOONG_NAMESPACES += \
-        vendor/qcom/opensource/libfmjni
-else ifeq ($(BOARD_HAVE_BCM_FM),true)
-    PRODUCT_SOONG_NAMESPACES += \
-        hardware/broadcom/fm
-else ifeq ($(BOARD_HAVE_SLSI_FM),true)
-    PRODUCT_SOONG_NAMESPACES += \
-        hardware/samsung_slsi/fm
-else ifneq ($(BOARD_HAVE_MTK_FM),true)
-    PRODUCT_SOONG_NAMESPACES += \
-        packages/apps/FMRadio/jni/fmr
-endif
