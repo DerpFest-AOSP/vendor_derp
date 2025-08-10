18757f36c8e79d8cdec849280c100bb7fdc78787
NurKeinNeid <mralexman3000@gmail.com>
2025-08-11 00:45:30 +0200

    config: Include certification repo
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- config/common.mk
+++ config/common.mk
@@ -203,6 +203,9 @@ PRODUCT_PACKAGE_OVERLAYS += \
 # Art
 include vendor/derp/config/art.mk
 
+# Certification
+$(call inherit-product, vendor/certification/config.mk)
+
 # Versioning
 include vendor/derp/config/version.mk
 
